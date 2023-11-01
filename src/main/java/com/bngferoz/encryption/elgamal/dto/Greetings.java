27c42899f3ebaa68f54ed52bff9b7156dd455a37
bngferoz <bngferoz@gmail.com>
2023-11-01 16:40:09 +0600

    initial commit

---
+++ src/main/java/com/bngferoz/encryption/elgamal/dto/Greetings.java
@@ -0,0 +1,14 @@
+package com.bngferoz.encryption.elgamal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greetings {
+	private String message;
+}
